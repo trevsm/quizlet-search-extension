041aaa90e7acefa545f3f44faee0ae43c1901b67
trevsm <trevsm@pm.me>
2022-09-25 17:06:18 -0600

    partial

--- index.js
+++ index.js
@@ -1,33 +1,23 @@
+const toggleModal = (e) => {
+  fetch('https://quizlet-search.herokuapp.com/', {
+    method: 'post',
+    body: e.target.value,
+  })
+    .then((r) => r.json())
+    .then((data) => {
+      console.log(data);
+    });
+};
+
 function main() {
-  alert('Hello, world!');
-
-  const questions = document.querySelectorAll('.question_text');
-
-  console.log(questions);
+  const quizletQuestions = document.querySelectorAll('.question_text');
 
   const element = document.createElement('div');
-  element.innerHTML = html`
-    <div class="quizlet-popup">
-      <button>search</button>
-    </div>
-  `;
+  element.innerText = 'search';
+  element.style.backgroundColor = 'pink';
+  element.addEventListener('click', toggleModal);
 
-  questions[0].appendChild(element);
-
-  const toggleModal = (e) => {
-    fetch('https://quizlet-search.herokuapp.com/', {
-      method: 'post',
-      body: e.target.value,
-    })
-      .then((r) => r.json())
-      .then((data) => {
-        console.log(data);
-      });
-  };
-
-  document
-    .querySelector('.quizlet-popup button')
-    .addEventListener('click', toggleModal);
+  quizletQuestions[0].appendChild(element);
 }
 
-addEventListener('load', main);
+window.addEventListener('load', main);
